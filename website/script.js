5dd4791614eec8f224b7208f572744abcc4ff881
Jeremy D <shazoum@hotmail.com>
2023-04-28 14:18:51 +0200

    added comments and instructions on html page

--- website/script.js
+++ website/script.js
@@ -7,7 +7,8 @@ var pnls = document.querySelectorAll('.panel').length,
 	scdir, hold = false;
 
 function _scrollY(obj) {
-	if(!canCreate){return;}
+	//block scrolling when in the new panel
+	if(!canCreatePanel){return;}
 	var slength, plength, pan, step = 100,
 		vh = window.innerHeight / 100,
 		vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
@@ -20,21 +21,24 @@ function _scrollY(obj) {
 	}
 	plength = plength || parseInt(pan.offsetHeight / vmin);
 	slength = parseInt(pan.style.transform.replace('translateY(', ''));
-	if (scdir === 'up' && Math.abs(slength) < (plength - plength / pnls)) {
+	//compute # pixels to scroll
+	if (scdir === 'down' && Math.abs(slength) < (plength - plength / pnls)) {
 		slength = slength - step;
-	} else if (scdir === 'down' && slength < 0) {
+	} else if (scdir === 'up' && slength < 0) {
 		slength = slength + step;
 	} else if (scdir === 'top') {
 		slength = 0;
 	}
+	//debounce wheel
 	if (hold === false) {
 		hold = true;
+		//scroll
 		pan.style.transform = 'translateY(' + slength + 'vh)';
 		setTimeout(function() {
 			hold = false;
 		}, 1000);
 	}
-	console.log(scdir + ':' + slength + ':' + plength + ':' + (plength - plength / pnls));
+	//console.log(scdir + ':' + slength + ':' + plength + ':' + (plength - plength / pnls));
 }
 
 /*[assignments]*/
@@ -42,10 +46,10 @@ var well = document.getElementById('well');
 well.style.transform = 'translateY(0)';
 well.addEventListener('wheel', function(e) {
 	if (e.deltaY < 0) {
-		scdir = 'down';
+		scdir = 'up';
 	}
 	if (e.deltaY > 0) {
-		scdir = 'up';
+		scdir = 'down';
 	}
 	e.stopPropagation();
 });
@@ -58,41 +62,49 @@ for (var i = 0; i < tops.length; i++) {
 	});
 }
 
-
+//when re-loading the page, scroll to the top
 window.onbeforeunload = function () {
 	window.scrollTo(0, 0);
 }
 
-var canCreate = true;
-async function createDiv(){
-	if(canCreate){
-		var divReference = document.getElementById('divCreator');
-		var divToCreate = document.createElement('div');
-		divToCreate.classList.add('panel');
-		divToCreate.setAttribute('id', 'panelToDelete');
-		divToCreate.innerHTML = "Lots of data for the patient !!!";
-		let func = "destroy('panelToDelete')"
-		divToCreate.innerHTML += '<br/><button onclick='+ func +'>Go back</button>';
-		divReference.parentNode.appendChild(divToCreate);
-		
 
-		scdir = 'up';
-		_scrollY(well);
-		canCreate = false;
-		await new Promise(resolve => setTimeout(resolve, 1000));
-	} else {
-		alert("Stop being greedy, you can create only one panel.")
-	}
+var canCreatePanel = true;
+//create a panel
+async function createDiv(){
+	//can create only one panel
+	if(!canCreatePanel){return;}
+	var divReference = document.getElementById('divCreator');
+	var divToCreate = document.createElement('div');
+	divToCreate.classList.add('panel');
+	divToCreate.setAttribute('id', 'panelToDelete');
+	divToCreate.innerHTML = "Lots of data for the patient !!!";
+	let func = "destroy('panelToDelete')"
+	divToCreate.innerHTML += '<br/><button onclick='+ func +'>Go back</button>';
+	divReference.parentNode.appendChild(divToCreate);
+	
+	//scroll down
+	scdir = 'down';
+	_scrollY(well);
+	canCreatePanel = false;
+	//let the animation play
+	await new Promise(resolve => setTimeout(resolve, 1000));
 }
 
+
+//destroy a created panel
 async function destroy(elem){
-	canCreate = true;
+	canCreatePanel = true;
+	//set the destination to top
 	scdir = 'top';
 	_scrollY(well);
-
+	let d = document.getElementById(elem);
+	const node = document.createTextNode("Go down");
+	d.parentNode.firstElementChild.appendChild(node);
+	//let the animation play
 	await new Promise(resolve => setTimeout(resolve, 1000));
 	
-	let d = document.getElementById(elem);
+	
+	
 	d.parentNode.removeChild(d);
 	
 }
