51b6a99098e1bf79ee77c89e98b0ad5ace6988dd
Léonard Vaney <leonard.vaney@epfl.ch>
2023-05-03 13:39:25 +0200

    first draft to filter basic data

--- website/script.js
+++ website/script.js
@@ -1,6 +1,66 @@
 //the basis of this script was found here: https://codepen.io/nearee/pen/zYYENMa
 
 
+d3.csv('http://localhost:8000/resources/data1/core/transfers.csv', createChart);
+//var data = d3.csv.parseRows('localhost:8000/resources/data1/core/transfers.csv');
+//console.log(data)
+//createChart(data)
+
+function createChart(data) {
+	
+	//var parseDate = d3.time.format("%Y-%m-%d").parse
+	const parseTime = d3.time.format("%Y-%m-%d %H:%M:%S").parse
+
+	var filtered_data = data.filter(d => {
+		return d.careunit != ""
+	});
+
+	filtered_data = filtered_data.sort((a,b) => {
+		return +a.subject_id - +b.subject_id
+	});
+
+	var subject_id_col = d3.map(filtered_data, d => {
+		return(d.subject_id)
+	}).keys();
+
+	const random_subject_index = Math.floor(Math.random() * subject_id_col.length);
+
+	filtered_data = filtered_data.filter(d => {
+		return d.subject_id == subject_id_col[random_subject_index]
+	});
+
+	filtered_data = d3.map(filtered_data, d => {
+		return([d.careunit, (parseTime(d.outtime) - parseTime(d.intime)) / 1000])
+	}).keys();
+
+	var acc = [["", 0]]
+	for(d in filtered_data){
+		if(acc.includes(d.careunit)){
+
+		}
+	}
+	//var test = (parseTime(filtered_data[0].outtime) - parseTime(filtered_data[0].intime)) / 1000 //Donne la valeur en seconde
+
+	/*var grouped_by_careunit = d3.flatRollup(
+		filtered_data,
+		x => ({
+		  intime: x.map(d => d.intime),
+		  outtime: x.map(d => d.outtime)
+		}),
+		d => d.careunit
+	  );*/
+	
+	//var grouped_by_careunit = d3.group(filtered_data, d => d.careunit)
+
+	/*filtered_data = d3.map(filtered_data, d => {
+
+	});*/
+
+	console.log(filtered_data)
+	/*console.log(parseTime(filtered_data[0].outtime))
+	console.log(parseTime(filtered_data[0].intime))*/
+}
+
 "use strict";
 /*[pan and well CSS scrolls]*/
 var pnls = document.querySelectorAll('.panel').length,
